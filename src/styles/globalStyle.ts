83eab5f72435584224797add79e2ffc550a55038
edmilson <ecmelo.26@gmail.com>
2025-08-31 11:53:05 -0300

    Feat: Form component

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,7 +1,6 @@
-import type { Theme } from '@/types'
 import { createGlobalStyle } from 'styled-components'
 
-export const GlobalStyle = createGlobalStyle<{ theme?: Theme }>`
+export const GlobalStyle = createGlobalStyle`
   body, html {
     background: ${props => props.theme.appBackground};
     color: ${props => props.theme.appColor};
@@ -9,7 +8,7 @@ export const GlobalStyle = createGlobalStyle<{ theme?: Theme }>`
     padding: 0;
     font-family: "Inter", sans-serif;
   }
-  h1, h2, p, ul, li {
+  h1, h2, p, ul, li, figure {
       margin: 0;
       padding: 0;
   }
