f7dc15cf0a49a9ddf0db8a94b449a8f0f7c0272f
edmilson <ecmelo.26@gmail.com>
2025-09-14 15:37:52 -0300

    Feat: Logout Flow

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,9 +1,11 @@
 import { createGlobalStyle } from 'styled-components'
+import type { DefaultTheme } from 'styled-components'
+import { pxToRem } from '@/utils'
 
-export const GlobalStyle = createGlobalStyle`
+export const GlobalStyle = createGlobalStyle< { theme?: DefaultTheme } > `
   body, html {
-    background: ${props => props.theme.appBackground};
-    color: ${props => props.theme.appColor};
+    background: ${(props) => props.theme.appBackground};
+    color: ${(props) => props.theme.appColor};
     margin: 0;
     padding: 0;
     font-family: "Inter", sans-serif;
@@ -11,5 +13,13 @@ export const GlobalStyle = createGlobalStyle`
   h1, h2, p, ul, li, figure {
       margin: 0;
       padding: 0;
+  }
+
+  .mb-1 {
+    margin-bottom: ${pxToRem(16)}
+  }
+    
+  .mb-2 {
+    margin-bottom: ${pxToRem(32)}
   }
 `
